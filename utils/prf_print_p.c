dc161c20f9a704d1ddc17fa93c2f84ce322032ad
Gustavo Carvalho <gustavo1net@hotmail.com>
2021-09-26 09:28:36 -0300

    remove treatment linux null pointer

--- utils/prf_print_p.c
+++ utils/prf_print_p.c
@@ -6,7 +6,7 @@
 /*   By: glima-de <glima-de@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/25 16:27:16 by glima-de          #+#    #+#             */
-/*   Updated: 2021/09/25 16:40:38 by glima-de         ###   ########.fr       */
+/*   Updated: 2021/09/26 09:28:04 by glima-de         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,6 @@
 
 int	prf_print_p(unsigned long pointer)
 {
-	if (pointer)
-	{
-		ft_putstr_fd("0x", 1);
-		return (prf_decToBase(pointer, "0123456789abcdef"));
-	}
-	ft_putstr_fd("(nil)", 1);
-	return (3);
+	ft_putstr_fd("0x", 1);
+	return (prf_decToBase(pointer, "0123456789abcdef"));
 }
